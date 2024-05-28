e8e6189cedd1925b49e143d5fe04872d3310b58e
Kanishk <113164601+Kanishkk-1@users.noreply.github.com>
2024-05-28 16:52:23 +0530

    Time: 2 ms (98.36%), Space: 18.4 MB (71.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int> mp;
-        int n = nums.size();
-        int q = n / 3;
-        vector<int>no;
-
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
+        int cnt1 =0;
+        int cnt2 =0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        int n=nums.size();
         
-        for (auto x: mp) {
+        int q = (int)(n/3)+1;
 
-            if (x.second > q) {
-                no.push_back(x.first);
+        for(int i=0;i<n;i++){
+            if (cnt1==0 && nums[i] != el2){
+                el1=nums[i];
+                cnt1=1;
+            }
+            else if (cnt2==0 && nums[i] != el1){
+                el2=nums[i];
+                cnt2=1;
+            }
+
+            else if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+
+            else {
+                cnt1--, cnt2--;
             }
         }
-        return no;
+       
+vector<int>result;
+       cnt1=0 ;cnt2=0;
+        for(int i=0; i<n;i++){
+            if(nums[i]== el1) cnt1++;
+            if(nums[i]== el2) cnt2++;
+        }
+
+        if (cnt1>=q) result.push_back(el1);
+        if (cnt2>=q) result.push_back(el2);
+     
+    sort(result.begin(), result.end());
+
+     return result;
     }
 };
