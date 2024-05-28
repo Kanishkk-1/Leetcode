3f87a0245610c9c1017258f87d654792fa0495de
Kanishk <113164601+Kanishkk-1@users.noreply.github.com>
2024-05-28 16:06:08 +0530

    Time: 3 ms (98.25%), Space: 18.5 MB (44.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-  vector<int> majorityElement(vector<int> &a) 
-  {
-	  int y(-1), z(-1), cy(0), cz(0);
-    
-	  for (const auto & x: a) 
-	  {
-		  if (x == y) cy++;
-		  else if (x == z) cz++;
-		  else if (! cy) y = x, cy = 1;
-		  else if (! cz) z = x, cz = 1;
-		  else cy--, cz--;
-     }
-      
-     cy = cz = 0;
-     for (const auto & x: a)
-		 if (x == y) cy++;
-			 else if (x == z) cz++;
-  
-	  vector<int> r;
-	  if (cy > size(a)/3) r.push_back(y);
-	  if (cz > size(a)/3) r.push_back(z);
-	  return r;
-  }
+    vector<int> majorityElement(vector<int>& nums) {
+        map<int, int> mp;
+        int n = nums.size();
+        int q = n / 3;
+        vector<int>no;
+
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+        
+        for (auto x: mp) {
+
+            if (x.second > q) {
+                no.push_back(x.first);
+            }
+        }
+        return no;
+    }
 };
