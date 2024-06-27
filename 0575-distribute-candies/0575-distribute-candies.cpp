c3ddeecaf2c5925703a10a9c71edbbd844e31d29
Kanishk <113164601+Kanishkk-1@users.noreply.github.com>
2024-06-27 19:50:12 +0530

    Time: 262 ms (7.84%), Space: 122.5 MB (35.05%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) 
+    {
+        map<int,int>m;
+        for(int i=0;i<candyType.size();i++)
+        {
+            m[candyType[i]]++;
+        }
+        int eat = candyType.size()/2;
+        int type = m.size();
+        if(eat>=type)
+        {
+            return type;
+        }
+        else
+        {
+            return eat;
+        }
+    }
+};
